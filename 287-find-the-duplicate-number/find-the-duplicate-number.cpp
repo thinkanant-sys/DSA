61d5d933b948344f8dee397ce5d45c134d2e27e8
thinkanant-sys <thinkanant@gmail.com>
2026-03-09 10:40:39 +0530

    Time: 7 ms (50.61%) | Memory: 65.3 MB (25.93%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,6 +4,7 @@ public:
         // array containing n+1 elements because one of the element is repeated i.e. duplicate
         //Approach-1 brute force -> O(n^2)
         //Approach-2 O(n) since numbers are in the range from[1,n] so we can create an array count   [n] and initialize it with 0 denoting all the elements appears 0 times initially now traverse the original array and store the element of array in a variable (let's say index) and then in the count array increment the index of array corresponding to the element of array variable as the array size is limited from [1,n]  
+        //leetsync
         int n=nums.size();
         int count[n+1]; // since the array index starts from 0 and we're mapping element with corresponding index i.e 0->0 1->1 5->5
 
