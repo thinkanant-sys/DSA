d742116c0e8da7bcf701bf6447710b864d146cf7
thinkanant-sys <thinkanant@gmail.com>
2026-03-09 11:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (77.98%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count_one=0,count_zero=0,count_two=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+            count_zero++;
+            else if(nums[i]==1)
+            count_one++;
+            else
+            count_two++;
+        }
+        for(int i=0;i<count_zero;i++){
+            nums[i]=0;
+        }
+        for(int i=count_zero;i<count_zero+count_one;i++){
+            nums[i]=1;
+        }
+        for(int i=count_zero+count_one;i<nums.size();i++){
+            nums[i]=2;
+        }
+    }
+};
