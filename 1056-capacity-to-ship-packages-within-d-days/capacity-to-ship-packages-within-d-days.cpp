69177e204e83bf7d8462dee9abdf7bdf08491284
thinkanant-sys <thinkanant@gmail.com>
2026-03-18 18:41:40 +0530

    Time: 11 ms (67.32%) | Memory: 35.2 MB (19.75%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         int start=weights[0],end=weights[0];
-        for(int i=1;i<weights.size();i++){
-            if(weights[i]>start)
-            start=weights[i];
+        for(int i=1;i<weights.size();i++){// calculating initial values of start and end
+            if(weights[i]>start)  // start-> maximum value in array
+            start=weights[i];    // end-> sum of all array values
             end+=weights[i];
         }
-        while(start<=end){
-            int mid=start+(end-start)/2;
+        while(start<=end){ 
+            int mid=start+(end-start)/2; // selecting capacity as mid
             int daysReq=days_req(weights,mid);
-            if(daysReq<=days)
-            end=mid-1;
-            else
-            start=mid+1;
+            if(daysReq<=days) // if days in which we can find out sum is less or equal
+            end=mid-1; // then checking for higher capacity as in less days we have shipped all parcels than the required days
+            else        
+            start=mid+1;  // if taking more days than given then we're increasing the capacity as with more capacity we can load more parcels
         }
         return start;
     }
     int days_req(vector<int>& weights, int caps){
-        int day=1,load=0;
+        int day=1,load=0;   // initially at day 1 load value is 0 
         for(int i=0;i<weights.size();i++){
             if(load+weights[i]>caps){
-                day++;
-                load=weights[i];
+                day++;  // incrementing days if load on ship is more than its capacity
+                load=weights[i]; // setting load value for new day
             }
             else
-            load+=weights[i];
+            load+=weights[i]; //adding weights if weight added to load is lesser than the capacity of the ship
         }
         return day;
     }
